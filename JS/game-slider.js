c56fc05cd8832adccb294973715af0027c37cc76
CBDA-creator <childbraindevelopmentacademy@outlook.com>
2025-10-24 02:24:53 +0000

    Second Version of the website with new changes

---
+++ JS/game-slider.js
@@ -0,0 +1,24 @@
+"use strict";
+
+const swiper = new Swiper(".mySwiper", {
+  slidesPerView: 3,
+  spaceBetween: 30,
+  loop: true,
+  autoplay: {
+    delay: 2500, // time between slides (in ms)
+    disableOnInteraction: false, // keep autoplay after user clicks or drags
+  },
+  pagination: {
+    el: ".swiper-pagination",
+    clickable: true,
+  },
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
+  },
+  breakpoints: {
+    0: { slidesPerView: 1 },
+    768: { slidesPerView: 2 },
+    1024: { slidesPerView: 3 },
+  },
+});
